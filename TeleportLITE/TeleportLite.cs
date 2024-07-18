ccf435009c2f11f04bf2a2d6e68fcc1f7fe02e51
sekainoue <sekainouexr@gmail.com>
2024-07-18 09:02:24 +0900

    Add project files.

---
+++ TeleportLITE/TeleportLite.cs
@@ -0,0 +1,71 @@
+﻿using ImGuiNET;
+using SharpPluginLoader.Core.Entities;
+using SharpPluginLoader.Core;
+
+namespace Teleport
+{
+    public class Teleport : IPlugin
+    {
+        public string Name => "FT Lite"; public string Author => "seka";
+        public Monster? _selectedMonsterT = null;
+        public void ResetState()
+        {
+            _selectedMonsterT = null;
+        }
+
+        private uint _lastStage = 0;
+        public void OnMonsterCreate(Monster monster)
+        {
+            _lastStage = (uint)Area.CurrentStage;
+        }
+        public void OnQuestLeave(int questId) => ResetState();
+        public void OnQuestComplete(int questId) => ResetState();
+        public void OnQuestFail(int questId) => ResetState();
+        public void OnQuestReturn(int questId) => ResetState();
+        public void OnQuestAbandon(int questId) => ResetState();
+        public void OnQuestEnter(int questId) => ResetState();
+        
+        public unsafe void OnImGuiRender()
+        {
+            var player = Player.MainPlayer;
+            if (player == null)
+                return;
+
+            var monsters = Monster.GetAllMonsters().TakeLast(5).ToArray();
+            if (monsters == null)
+                return;
+            if (ImGui.BeginCombo("Select", $"{_selectedMonsterT}"))
+            {
+                foreach (var monster in monsters)
+                {
+                    if (ImGui.Selectable($"{monster}", _selectedMonsterT == monster))
+                    {
+                        _selectedMonsterT = monster;
+                    }
+                }
+                ImGui.EndCombo();
+            }
+
+            if (ImGui.Button("You-to-Mon"))
+            {
+                if (_selectedMonsterT == null || player == null)
+                    return;
+                player.Teleport(_selectedMonsterT.Position);
+            }
+        }
+
+        public unsafe void OnUpdate(float deltaTime)
+        {
+            var player = Player.MainPlayer;
+            if (player is null)
+                return;
+            if ((uint)Area.CurrentStage != _lastStage)
+            {
+                ResetState();
+            }
+
+            uint stageID = (uint)Area.CurrentStage;
+           
+        }
+    }
+}
